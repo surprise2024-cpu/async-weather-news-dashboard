b74537a4586db7a37ecb54f6ffa2ecfc0950cac7
surprise2024-cpu <surprise2024@gmail.com>
2026-09-15 21:42:51 +0200

    feat: initialize display module with API imports and type definitions

---
+++ src/display.ts
@@ -0,0 +1,15 @@
+import * as https from 'https'
+
+import {
+    WEATHER_API_URL,
+    LOCATION_NAME,
+    NEWS_API_URL
+} from './config'
+
+import type {
+    NewsApiResponse,
+    NewsPost,
+    WeatherApiResponse,
+    WeatherData
+} from './types'
+
