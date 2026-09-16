d82304e72249a5e64c4b617f5a4b09283fb2a8d3
surprise2024-cpu <surprise2024@gmail.com>
2026-09-16 05:45:26 +0200

    feat: update API URLs for weather and geocoding services

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 // storing weather data from API into variable
+export const GEOCODING_API_URL = 
+    "https://geocoding-api.open-meteo.com/v1/search";
+
 export const WEATHER_API_URL = 
-    "https://api.open-meteo.com/v1/forecast?latitude=-25.7479&longitude=28.2293&current=temperature_2m,apparent_temperature,wind_speed_10m,weather_code";
+    "https://api.open-meteo.com/v1/forecast";
 
 // storing news data from API into variable
 export const NEWS_API_URL = 
     "https://dummyjson.com/posts?limit=5";
-
-// location related to the data stored from APIs
-export const LOCATION_NAME = "Pretoria";
